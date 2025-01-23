5b349cea7f3bd9d83d9225b901b317cf09e1b1ec
hjHwang <hhyejin90@naver.com>
2025-01-23 15:40:46 +0900

    [chore] theme.jsx의 color Object styled-component 적용 테스트 완료

--- src/router.jsx
+++ src/router.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage/LandingPage";
+// import LandingPage from "./pages/LandingPage/LandingPage";
 import ListPage from "./pages/ListPage/ListPage";
 import ToPage from "./pages/ToPage/ToPage";
 import RollingPage from "./pages/RollingPage/RollingPage";
 import FromPage from "./pages/FromPage/FromPage";
 import Layout from "./styles/Layout";
+import Test from "./styles/Test";
 export default function Routers() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<LandingPage />}></Route>
+          <Route index element={<Test />}></Route>
           <Route path="list" element={<ListPage />}></Route>
           <Route path="post" element={<ToPage />}></Route>
           <Route path="post:id" element={<RollingPage />}></Route>
