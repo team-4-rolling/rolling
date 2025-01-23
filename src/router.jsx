21171c6bd563ab8b8365e207ee2f687555b4a554
hjHwang <hhyejin90@naver.com>
2025-01-23 16:24:39 +0900

    [chore] delete test file

--- src/router.jsx
+++ src/router.jsx
@@ -1,17 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import LandingPage from "./pages/LandingPage/LandingPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
 import ListPage from "./pages/ListPage/ListPage";
 import ToPage from "./pages/ToPage/ToPage";
 import RollingPage from "./pages/RollingPage/RollingPage";
 import FromPage from "./pages/FromPage/FromPage";
 import Layout from "./styles/Layout";
-import Test from "./styles/Test";
 export default function Routers() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Test />}></Route>
+          <Route index element={<LandingPage />}></Route>
           <Route path="list" element={<ListPage />}></Route>
           <Route path="post" element={<ToPage />}></Route>
           <Route path="post:id" element={<RollingPage />}></Route>
