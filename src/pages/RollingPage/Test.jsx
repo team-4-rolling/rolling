747e3ebc52a8546e1b791ce54c12b3b922cc7f1d
nerte <grimza@nerteui-MacBookPro.local>
2025-01-26 01:55:22 +0900

    [Fix] id값을 useParams로 받아와 전달하자 무한로딩이 안되는 오류 발생 해결완료

--- src/pages/RollingPage/Test.jsx
+++ src/pages/RollingPage/Test.jsx
@@ -1,6 +1,6 @@
 import * as S from "./TestStyle";
 import { useCallback, useEffect, useState } from "react";
-import { getMessage, getRecipientsId } from "./TestApi";
+import { getMessage, getRecipients } from "./TestApi";
 import Test3 from "./Test3";
 import { throttle } from "lodash";
 import Button from "../../components/common/Button/Button.jsx";
@@ -9,7 +9,7 @@ import { useParams } from "react-router-dom";
 
 function Test() {
   //useParams 테스트 부분
-  const { id } = useParams();
+  const { id: queryId } = useParams();
 
   //
   const [messages, setMessages] = useState([]);
@@ -17,48 +17,52 @@ function Test() {
   const [isScrollEnd, setIsScrollEnd] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [isEdit, setIsEdit] = useState(false);
+  const [hasNext, setHasNext] = useState(true);
   //
 
   //
   const handleLoad = async () => {
-    const recipientId = await getRecipientsId(id);
+    const { id } = await getRecipients(queryId);
     setIsLoading(true);
     let limit = offset === 0 ? 8 : 9;
-    const { results } = await getMessage(limit, offset, recipientId);
+    const { results, next } = await getMessage(limit, offset, id);
     if (!results) return;
     setMessages((prevMessages) => [...prevMessages, ...results]);
     setOffset(offset + limit);
+    setIsLoading(false);
+    setHasNext(next);
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", infiniteScroll);
     if (!isLoading) {
       handleLoad();
-      setIsLoading(false);
     }
-
+    if (hasNext) {
+      console.log("이벤트 등록");
+      window.addEventListener("scroll", infiniteScroll);
+    }
     return () => {
       infiniteScroll.cancel();
       window.removeEventListener("scroll", infiniteScroll);
+      console.log("이벤트 삭제");
     };
   }, [isScrollEnd]);
 
-  const handelEditClick = () => {
-    setIsEdit(true);
-  };
   const infiniteScroll = useCallback(
     throttle(() => {
       if (!isLoading) {
         const { clientHeight, scrollHeight, scrollTop } =
           document.documentElement;
         if (clientHeight + scrollTop >= scrollHeight - 1) {
-          setIsScrollEnd((prev) => !prev); // 스크롤이 가장 하단일때 isScroll값을 반대로 바꿈
+          setIsScrollEnd((prev) => !prev);
         }
       }
     }, 200),
     []
   );
-
+  const handelEditClick = () => {
+    setIsEdit(true);
+  };
   const handelDeleteClick = () => {
     setIsEdit(false);
   };
--- src/pages/RollingPage/TestApi.jsx
+++ src/pages/RollingPage/TestApi.jsx
@@ -1,9 +1,9 @@
 const BASE_URL = "https://rolling-api.vercel.app/13-4/recipients";
 
 //해당 수신자를 id로 받아 수신자의 메시지와 그 정보들을 body로 리턴하는 리퀘스트
-export async function getMessage(limit, offset, recipientId) {
+export async function getMessage(limit, offset, id) {
   const response = await fetch(
-    `${BASE_URL}/${recipientId}/messages/?limit=${limit}&offset=${offset}`
+    `${BASE_URL}/${id}/messages/?limit=${limit}&offset=${offset}`
   );
   if (!response.ok) {
     console.log("api 실패");
@@ -14,22 +14,14 @@ export async function getMessage(limit, offset, recipientId) {
   return body;
 }
 // 롤링페이퍼 전체 수신자가 담긴 results 를 리턴하는 리퀘스트
-export async function getRecipientsId(id) {
+// 희진님 nav에서 이 함수의 리턴에서 사용하시면 됩니다.
+export async function getRecipients(queryId) {
   const response = await fetch(`${BASE_URL}/?limit=0&offset=0`);
   if (!response.ok) {
     console.log("recipients 조회 실패");
     throw new Error("getRecipientsId 실패");
   }
   const body = await response.json();
-  const { results } = body;
-  const { id: recipientId } = results.find((recipient) => recipient.id == id);
-  return recipientId;
+  const recipient = body.results.find((recipient) => recipient.id == queryId);
+  return recipient;
 }
-
-/**
- * 0. id값에 따라서 데이터를 가져올수 있게 일단 구현 00
- * 1. id 값을 useParams로 가져옴
- * 2. 그 id값을 getMessage로 넘겨서 해당 id의 메시지를 받아옴
- * 3. 그 메시지를 페이지에 뿌려줌
- * 4.
- */
