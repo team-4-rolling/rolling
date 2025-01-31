fb8a64d296bc165b8813ff239f85cf8fa635b3c4
heejin <heejin0857@naver.com>
2025-02-01 01:14:10 +0900

    [Fix] image length 3 이하인 경우 예외처리

--- src/components/common/Header/SecondHeader.jsx
+++ src/components/common/Header/SecondHeader.jsx
@@ -18,7 +18,9 @@ export default function SecondHeader({ name, messageCount, recentMessages }) {
           <S.ToUser>To. {name}</S.ToUser>
           <S.Div>
             <S.WrittenContainer>
-              {images.length !== 0 && <CircleImages writtenCount={messageCount} images={images} />}
+              {images.length !== 0 && (
+                <CircleImages messageCount={messageCount} images={images} />
+              )}
               <S.Written>
                 <span>{messageCount}</span>명이 작성했어요!
               </S.Written>
@@ -27,14 +29,21 @@ export default function SecondHeader({ name, messageCount, recentMessages }) {
               <S.Icons></S.Icons>
               <S.Btns>
                 <S.Emoji>
-                  <Button outlineMedium $font={`${theme.font.H5Regular}`} style={{ cursor: "pointer", width: "100%" }}>
+                  <Button
+                    outlineMedium
+                    $font={`${theme.font.H5Regular}`}
+                    style={{ cursor: "pointer", width: "100%" }}
+                  >
                     <img src={smile} />
                     <span>추가</span>
                   </Button>
                 </S.Emoji>
                 <Line width="1px" height="28px" />
                 <S.Share>
-                  <Button outlinesmall style={{ cursor: "pointer", width: "100%" }}>
+                  <Button
+                    outlinesmall
+                    style={{ cursor: "pointer", width: "100%" }}
+                  >
                     <img src={share} />
                   </Button>
                 </S.Share>
