32d91dbe9c9a4da529ca1f78e03ff9f49530a0c2
nerte <grimza@nerteui-MacBookPro.local>
2025-01-31 17:25:54 +0900

    [Chore] delete useEffect

--- src/pages/RollingPage/Messages.jsx
+++ src/pages/RollingPage/Messages.jsx
@@ -3,15 +3,17 @@ import * as S from "./RollingPage.style";
 import PostCardUI from "../../components/RollingCard/RollingCard";
 import PlusIcon from "../../assets/icons/PlusIcon.svg";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 //
 
 export default function Message({ isEdit, messages }) {
-  const [filterMessages, setFilterMessages] = useState();
+  const [filterMessages, setFilterMessages] = useState(messages);
+
   const handleClickFilter = (e) => {
     const disappear = filterMessages.filter(
       (message) => message.id != e.currentTarget.dataset.value
     );
+    setFilterMessages(disappear);
   };
   //
   return (
