5b48f8c2de429053ea3f4dc9a9f6e9bca9b5b695
heejin <heejin0857@naver.com>
2025-02-02 21:14:10 +0900

    [Chore] env base url edit

--- src/api/reactions.jsx
+++ src/api/reactions.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `${import.meta.env.VITE_BASE_URL}/13-4/recipients`;
+const BASE_URL = `${import.meta.env.VITE_BASE_URL}13-4/recipients`;
 
 export async function getReactions(recipientsId) {
   const response = await axios.get(`${BASE_URL}/${recipientsId}/reactions/`);
