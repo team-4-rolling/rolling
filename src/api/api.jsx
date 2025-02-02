735be339e606610d45b12474ec4e267edadb0420
ozen0718 <limoyng301p@naver.com>
2025-02-02 13:43:59 +0900

    [Style] Button css 분리

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -21,7 +21,7 @@ export async function getRecipients(queryId) {
     backgroundColor: color,
     backgroundImageURL: img,
     messageCount,
-    recentMessages
+    recentMessages,
   } = recipient;
   return { id, name, color, img, messageCount, recentMessages };
 }
