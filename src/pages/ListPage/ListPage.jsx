ca46f8032eee78dd4a9243951f1373837cc3ad6a
ozen0718 <limoyng301p@naver.com>
2025-02-03 18:33:08 +0900

    [Style] 버튼 스타일 추가조정04

--- src/pages/ListPage/ListPage.jsx
+++ src/pages/ListPage/ListPage.jsx
@@ -1,26 +1,7 @@
-import Button from "../../components/common/Button/Button";
-import smail from "../../assets/icons/smile.svg";
-import share from "../../assets/icons/share.svg";
-
 export default function ListPage() {
   return (
     <>
-      <Button>확인</Button>
-      <br />
-      <Button medium>확인</Button>
-      <br />
-      <Button large>확인</Button>
-      <br />
-      <Button outline>확인</Button>
-      <br />
-      <Button outlineMedium>
-        <img src={smail} />
-        추가
-      </Button>
-      <br />
-      <Button outlineSmall>
-        <img src={share} />
-      </Button>
+      <h1>hi</h1>
     </>
   );
 }
