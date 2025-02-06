7bbd740d83665ab0dae579653dcd2e89112dee03
nerte <grimza@nerteui-MacBookPro.local>
2025-02-07 00:32:38 +0900

    [Chore] edit constants

--- src/constants/messageConstants.jsx
+++ src/constants/messageConstants.jsx
@@ -3,16 +3,16 @@ export const TOAST_TEXT = {
   SUCCESS_CREATE_MESSAGE: "메시지를 성공적으로 보냈어요!",
   FAIL_CREATE_MESSAGE: "메시지 전송에 실패했어요.",
   //롤링페이퍼 생성
-  SUCCESS_CREATE_PAPER: "님의 롤링페이퍼 생성에 성공 했습니다!",
-  FAIL_CREATE_PAPER: "롤링페이퍼 생성에 실패 했습니다!",
+  SUCCESS_CREATE_PAPER: "님의 롤링 페이퍼 생성에 성공했습니다!",
+  FAIL_CREATE_PAPER: "롤링 페이퍼 생성에 실패 했습니다!",
   //idPost 페이지
-  FAIL_GET_MESSAGE: "메시지를 불러올수 없습니다. 다시 시도해 주세요",
-  FAIL_GET_RECIPIENT: "해당 롤링페이퍼를 찾을 수 없습니다.",
+  FAIL_GET_MESSAGE: "메시지를 불러 올 수 없습니다. 다시 시도해 주세요",
+  FAIL_GET_RECIPIENT: "해당 롤링 페이퍼를 찾을 수 없습니다.",
   SUCCESS_DELETE_MESSAGE: "메시지 삭제 완료!",
   FAIL_DELETE_MESSAGE: "메시지 삭제 완료!",
   HAVE_NO_DELETE_MESSAGE: "삭제할 메시지를 선택해주세요!",
-  SUCCESS_DELETE_PAGE: "롤링페이퍼가 삭제되었습니다!",
-  FAIL_DELETE_PAGE: "해당 롤링페이퍼를 삭제할 수 없습니다.",
+  SUCCESS_DELETE_PAGE: "롤링 페이퍼가 삭제 되었습니다!",
+  FAIL_DELETE_PAGE: "해당 롤링 페이퍼를 삭제할 수 없습니다.",
 };
 
 export const BUTTON_LABELS = {
@@ -24,10 +24,10 @@ export const BUTTON_LABELS = {
   CANCEL: "취소",
   CONFIRM: "확인",
   DELETE_REQUEST: "저장하기",
-  PAGE_DELETE: "롤링페이퍼 삭제하기",
+  PAGE_DELETE: "롤링 페이퍼 삭제하기",
   MESSAGE_DELETE: "메시지 삭제하기",
 };
 
 export const MODAL_TEXT = {
-  DELETE_CONFIRM: "정말 이 롤링페이퍼를 삭제하시겠어요?",
+  DELETE_CONFIRM: "정말 이 롤링 페이퍼를 삭제하시겠어요?",
 };
